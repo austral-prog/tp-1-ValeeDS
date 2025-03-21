094a2fabdebf4aaff05dc79b52f847dcbf1bb911
Valeria Del Savio <valeria.delsavio@hotmail.com>
2025-03-21 12:05:30 -0300

    commit exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,19 @@
 def math():
     a = 57
     b = 7
+
+    suma = a + b
+    diferencia = a - b
+    prod = a * b
+    prom = suma / 2
+    coc_entero = a // b
+    resto = a % b
+    cociente_real = a / b
+
+    print(suma)
+    print(diferencia)
+    print(prod)
+    print(prom)
+    print(coc_entero)
+    print(resto)
+    print(cociente_real)
